df90dcde20fe16d4e0a77a8d6aaf1d83ae2bf73c
domhanak <domin.hanak@gmail.com>
2015-04-13 17:40:44 +0200

    made controller for modalInstance work

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -20,65 +20,64 @@ visualizerControllers.controller('SystemListCtrl', ['$scope','System',
  * Controller that handles control of detailed-view of system.
  * It fetches desired system using service and assigns them to model.
  */
-visualizerControllers.controller('SystemDetailCtrl',['$scope', '$routeParams', '$modal', 'System',
-    function($scope, $routeParams, System){
+visualizerControllers.controller('SystemDetailCtrl',['$scope', '$routeParams','System', '$modal',
+    function($scope, $routeParams, System, $modal){
         $scope.system = System.get({systemId: $routeParams.systemId});
         $scope.currentIteration = 0;
         $scope.orderProp = 'id';
 
-        $scope.$watch('selected_machine', function(oldVal, newVal) {
-            if (oldVal != newVal) {
-                $scope.selected_machine = newVal;
-            }
-        });
-
         $scope.open = function (machine) {
-            var modalInstance = $modal.open({
-                templateUrl: 'machine-modal.ejs',
+             var modalInstance = $modal.open({
+                templateUrl: 'views/partials/machine-modal.ejs',
                 controller: 'MachineModalCtrl',
                 resolve: {
                     addedMachine: function() {
                         return machine;
                     }
                 }
-            })
-        }
+            });
 
-        modalInstance.result.then(function (addedMachine) {
-            $scope.system.machines.push(addedMachine);
-        });
+            modalInstance.result.then(function (addedMachine) {
+                $scope.currentIteration = addedMachine.addTime;
+            });
+        };
     }]);
 
 visualizerControllers.controller('MachineModalCtrl',
     function ($scope, $modalInstance, addedMachine) {
         $scope.addedMachine = addedMachine;
 
-
         $scope.submit = function () {
             $modalInstance.close($scope.addedMachine);
-        }
+        };
 
         $scope.cancel = function() {
             $modalInstance.dismiss('cancel');
-        }
+        };
 
         $scope.editQuery = function(queryId) {
             if (queryId === 'new') {
                 $scope.edit = true;
-                $scope.incomplete = true;
+                $scope.incomplete = false;
                 $scope.iQuery = '';
             } else {
-                $scope.edit = false;
-                $scope.iQuery = $scope.addedMachine.queries[id-1].query;
+                $scope.edit = true;
+                $scope.incomplete= false;
+                $scope.iQuery = addedMachine.queries[queryId];
             }
-        }
+        };
 
         $scope.saveQuery = function () {
-            var nuQuery = []
+            var nuQuery = {};
             nuQuery.query = $scope.iQuery;
-            nuQuery.id = addedMachine.queries.size();
-            nuQuery.addTime = $scope.$parent.currentIteration;
+            nuQuery.addTime = addedMachine.addTime;
 
-            addedMachine.push(nuQuery);
-        }
+            if (addedMachine.queries != null) {
+                addedMachine.queries.push(nuQuery);
+            } else {
+                var queries = [];
+                queries.push(nuQuery);
+                addedMachine.queries = queries;
+            }
+        };
 });
