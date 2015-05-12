0d8e8af3d1d21c039b607873e1beb71d025caebf
dhanak <domin.hanak@gmail.com>
2015-05-12 09:09:11 +0200

    add documentation

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -1,10 +1,13 @@
 'use strict';
 /**
- * Services for getting systems.
+ * Services . This file contains all services used bz application.
  */
 
+//defining ngResource dependency
 var visualizerServices = angular.module('visualizerServices', ['ngResource']);
 
+
+// service for fetching systems from server
 visualizerServices.factory('System', ['$resource',
     function($resource){
         return $resource('/systems/:systemId', null, {
